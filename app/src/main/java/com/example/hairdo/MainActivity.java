a7227f7722fcb3c9082039f5956d36002ec5df4a
Nirojan21 <nirojan.yoga@gmail.com>
2021-08-09 22:57:12 +0530

    login page ui design finished

--- app/src/main/java/com/example/hairdo/MainActivity.java
+++ app/src/main/java/com/example/hairdo/MainActivity.java
@@ -24,6 +24,6 @@ public class MainActivity extends AppCompatActivity {
                         finish();
                     }
                 },
-                1000);
+                2000);
     }
 }
