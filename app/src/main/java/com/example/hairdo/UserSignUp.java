a7db955d0b1cf4568a2cfb6e6ef05eee1d518fc3
Nirojan21 <nirojan.yoga@gmail.com>
2021-08-10 14:12:28 +0530

    signUp pages finished

---
+++ app/src/main/java/com/example/hairdo/UserSignUp.java
@@ -0,0 +1,16 @@
+package com.example.hairdo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_up);
+
+        getSupportActionBar().hide();
+    }
+}
