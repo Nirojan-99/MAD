b818849813aa675b5ebc6ebc918644c1aacd14a6
Sayanthan <kathirsayanthan@gmail.com>
2021-08-13 22:04:32 +0530

    finish the appointment_summary and cancel_upcoming_appointment ui

---
+++ app/src/main/java/com/example/hairdo/appointment_summary.java
@@ -0,0 +1,40 @@
+package com.example.hairdo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class appointment_summary extends AppCompatActivity {
+    ArrayList<String> salonName = new ArrayList<>();
+    ArrayList<String> date = new ArrayList<>();
+    ArrayList<String> time = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_summary);
+
+        salonName.add("salonName");
+        salonName.add("salonName");
+        salonName.add("salonName");
+
+        date.add("12.08.2021");
+        date.add("12.08.2021");
+        date.add("12.08.2021");
+
+        time.add("2.30AM");
+        time.add("2.30AM");
+        time.add("2.30AM");
+
+        RecyclerView recyclerView = findViewById(R.id.appointment_summary_recycleview2);
+        appointment_summaryAdpter Adapter2 = new appointment_summaryAdpter(salonName,date,time);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(Adapter2);
+
+
+    }
+}
